8c98cf359e08fed3ba08ce651f67a0633e1a9b90
Travis <travisredden@outlook.com>
2023-04-23 12:23:03 -0500

    added keyboard generator

--- src/components/Home/components/Keyboard.tsx
+++ src/components/Home/components/Keyboard.tsx
@@ -1,7 +1,41 @@
+import { QWERTY } from '@/assets/QERTY';
+import KeyRowGenerator from '@/components/Keyboard/KeyRowGenerator';
+import { Box } from '@mui/material';
 import React from 'react';
 
 const Keyboard = () => {
-  return <> be a keyboard eventually</>;
+  //TODO: set this in context
+  const board = QWERTY;
+  const color: { [key: string]: string } = {
+    0: 'red',
+    1: 'yellow',
+    2: 'green',
+    3: 'blue',
+    4: 'pink',
+    5: 'purple',
+    6: 'orange',
+  };
+  const content = board.map((row, index) => {
+    const clr: string = color[index];
+    return (
+      <div key={index} style={{ backgroundColor: `${color[index]}` }}>
+        <KeyRowGenerator key={index} keyRow={row} />
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <Box
+        display='flex'
+        flexDirection='column'
+        width='100vw'
+        justifyContent='center'
+        alignItems='center'>
+        {content}
+      </Box>
+    </>
+  );
 };
 
 export default Keyboard;
--- src/components/Keyboard/KeyRowGenerator.tsx
+++ src/components/Keyboard/KeyRowGenerator.tsx
@@ -1,8 +1,29 @@
-import { KeyBoardLayoutType } from '@/types/keyboardTypes';
-import React from 'react';
+import { KeyBoardLayoutType, KeyCode } from '@/types/keyboardTypes';
+import { Grid } from '@mui/material';
+import React, { ReactElement } from 'react';
 
-const KeyRowGenerator = (board: KeyBoardLayoutType): JSX.Element => {
-  return <></>;
+interface props {
+  keyRow: KeyCode[];
+}
+
+const KeyRowGenerator = ({ keyRow }: props): ReactElement => {
+  let row: JSX.Element[] = [];
+
+  row = keyRow.map((key) => {
+    const legend = key?.key;
+    const code = key?.code;
+    return (
+      <Grid height='4rem' width='4rem' key={`${legend}, ${code}`}>
+        {legend}
+      </Grid>
+    );
+  }) as JSX.Element[];
+
+  return (
+    <Grid display='flex' flexDirection='row' width='100%'>
+      {row}
+    </Grid>
+  );
 };
 
 export default KeyRowGenerator;
